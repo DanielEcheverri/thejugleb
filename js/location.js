422e8dbb963734dfb3b6184de958d1dc9686e926
danielecheverri <danielckx@gmail.com>
2025-11-30 14:33:46 +0100

    Separated NFC functions from location.js

--- js/location.js
+++ js/location.js
@@ -78,38 +78,38 @@ function calculateDistance(lat1, lon1, lat2, lon2) {
 	return distance;
 }
 
-async function readTag() {
-	try {
-		if (tagID === previousTagID || tagID === 0 || speaking) {
-			return;
-		} else {
-			const tagInfo = await fetchJSONFile(tagSource, cachedTagInfo); // from main.js
+// async function readTag() {
+// 	try {
+// 		if (tagID === previousTagID || tagID === 0 || speaking) {
+// 			return;
+// 		} else {
+// 			const tagInfo = await fetchJSONFile(tagSource, cachedTagInfo); // from main.js
 
-			if (tagInfo && tagInfo.tags.hasOwnProperty(tagID)) {
-				previousTagID = tagID;
-				window.success_Sound.play();
-				console.log("--Tag read: " +tagID);
+// 			if (tagInfo && tagInfo.tags.hasOwnProperty(tagID)) {
+// 				previousTagID = tagID;
+// 				window.success_Sound.play();
+// 				console.log("--Tag read: " +tagID);
 
-				const tagData = tagInfo.tags[tagID];
+// 				const tagData = tagInfo.tags[tagID];
 
-				if (tagInfo.tags[tagID].function) {
-					eval(tagInfo.tags[tagID].function);
-				}
-				if (tagData.variable && typeof tagData.boolean === 'boolean') {
-					window[tagData.variable] = tagData.boolean;
-				}
-				displayText(tagData.displayText); // from main.js
+// 				if (tagInfo.tags[tagID].function) {
+// 					eval(tagInfo.tags[tagID].function);
+// 				}
+// 				if (tagData.variable && typeof tagData.boolean === 'boolean') {
+// 					window[tagData.variable] = tagData.boolean;
+// 				}
+// 				displayText(tagData.displayText); // from main.js
 
-			} else {
-				window.alert_Sound.play();
-				console.error('Tag ' + tagID + ' information not found.');
-				return;
-			}
-		}
-	} catch (err) {
-		console.error('Error reading tag', err);
-	}
-}
+// 			} else {
+// 				window.alert_Sound.play();
+// 				console.error('Tag ' + tagID + ' information not found.');
+// 				return;
+// 			}
+// 		}
+// 	} catch (err) {
+// 		console.error('Error reading tag', err);
+// 	}
+// }
 
 async function checkCheckpoints() {
 	const radius = 12; // 12 meters radius
---
+++ js/nfc.js
@@ -0,0 +1,32 @@
+async function readTag() {
+	try {
+		if (tagID === previousTagID || tagID === 0 || speaking) {
+			return;
+		} else {
+			const tagInfo = await fetchJSONFile(tagSource, cachedTagInfo); // from main.js
+
+			if (tagInfo && tagInfo.tags.hasOwnProperty(tagID)) {
+				previousTagID = tagID;
+				window.success_Sound.play();
+				console.log("--Tag read: " +tagID);
+
+				const tagData = tagInfo.tags[tagID];
+
+				if (tagInfo.tags[tagID].function) {
+					eval(tagInfo.tags[tagID].function);
+				}
+				if (tagData.variable && typeof tagData.boolean === 'boolean') {
+					window[tagData.variable] = tagData.boolean;
+				}
+				displayText(tagData.displayText); // from main.js
+
+			} else {
+				window.alert_Sound.play();
+				console.error('Tag ' + tagID + ' information not found.');
+				return;
+			}
+		}
+	} catch (err) {
+		console.error('Error reading tag', err);
+	}
+}
