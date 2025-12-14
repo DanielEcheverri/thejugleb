f35edc7b6aea074ec1a34361f9971f33787f7483
danielecheverri <danielckx@gmail.com>
2025-12-14 20:37:31 +0100

    dmdmd

--- dashboard/all.js
+++ dashboard/all.js
@@ -1,1 +1,10 @@
 console.log("Loaded");
+
+// 13. Function to stop making comments for a character
+function stopComments(character) {
+    if (characterData[character]) {
+        clearInterval(characterData[character].intervalId);
+        delete characterData[character];
+    }
+    console.log("Stopping comments");
+}
