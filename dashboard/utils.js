e78e9d748f0a18793172f02e4ac089fd9e478e6e
danielecheverri <danielckx@gmail.com>
2026-02-02 17:35:24 +0100

    dkldlkd

--- dashboard/utils.js
+++ dashboard/utils.js
@@ -247,11 +247,11 @@ window.loadScene = function(passageName) {
             // 2. Set a flag telling the passage that data is ready
             SugarCube.State.variables.sceneReady = true;
             // 3. Force the passage to re-render immediately
-            window.SugarCube.UI.update();
+            SugarCube.Engine.show();
         })
         .catch(error => {
             console.error("Could not load " + url, error);
             SugarCube.State.variables.sceneError = true;
-            window.SugarCube.UI.update();
+            SugarCube.Engine.show();
         });
 };
